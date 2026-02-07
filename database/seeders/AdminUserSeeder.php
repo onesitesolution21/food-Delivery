6f0e46e659c38334f052544851b131a8e522fbf8
onesitesolution21 <onesitesolution21@gmail.com>
2026-02-07 13:23:01 +0000

    Create AdminUserSeeder.php
    data seeder added

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminUserSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the admin user.
+     */
+    public function run(): void
+    {
+        User::firstOrCreate(
+            ['email' => 'admin@shop.com'],
+            [
+                'name' => 'Admin User',
+                'password' => Hash::make('password'),
+            ]
+        );
+
+        User::firstOrCreate(
+            ['email' => 'test@example.com'],
+            [
+                'name' => 'Test User',
+                'password' => Hash::make('password'),
+            ]
+        );
+    }
+}
